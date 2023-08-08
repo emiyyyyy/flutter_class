38b1e36c2ffc76f18be2c9de9aba0d2ad00dc391
Jack Wagner <Jackwagner654@gmail.com>
2023-08-08 11:06:25 -0700

    avialableTeachers Update

--- lib/availableTeachers.dart
+++ lib/availableTeachers.dart
@@ -5,9 +5,10 @@ import 'package:flutter_class/teacherAccount.dart';
 import 'authentication.dart';
 
 class availableTeachers extends StatefulWidget {
-
+  late final String courseName;
+  availableTeachers(this.courseName);
   @override
-  State<availableTeachers> createState() => availableTeachersState();
+  State<availableTeachers> createState() => availableTeachersState(courseName);
 }
 
 
@@ -15,28 +16,32 @@ class availableTeachersState extends State<availableTeachers> {
   FirebaseFirestore db = FirebaseFirestore.instance;
   AuthenticationHelper Auth = AuthenticationHelper();
   List<String> candidates = [];
+  late final String courseName;
+
   List<String> teacherImages = [
     "https://cdn.discordapp.com/attachments/1070956419949535272/1134368270301016064/istockphoto-75940775-612x612.jpg",
     "https://cdn.discordapp.com/attachments/1070956419949535272/1134368270301016064/istockphoto-75940775-612x612.jpg",
     "https://cdn.discordapp.com/attachments/1070956419949535272/1134368270301016064/istockphoto-75940775-612x612.jpg"
   ];
 
-  void refreshCandidates() {
-    db.collection("users.Teachers").get().then((querySnapshot) {
+  availableTeachersState(this.courseName);
+
+
+  void refreshTeachers() {
+    db.collection("Courses").doc(courseName).get().then((value) {
       List<String> tmpNames = [];
       List<String> tmpImg = [];
-      for (var i in querySnapshot.docs) {
-        //   print(i.data());
-        //   print(i.id);
-        //    print("hello");
-        setState(() {
-          tmpNames.add(i.data()["Teachers"]["name"]);
-          // tmpImg.add(i.data()["Teachers"]["image"]);
+      Map<String, dynamic> data = value.data() as Map<String, dynamic>;
+      for (var i in data.keys) {
+        db.collection("users.Teachers").doc(i).get().then((value) {
+          print(value.data());
         });
-
-
       }
-      candidates = tmpNames;
+
+
+
+
+
       //teacherImages = tmpImg;
     });
   }
@@ -50,7 +55,7 @@ class availableTeachersState extends State<availableTeachers> {
 
   @override
   Widget build(BuildContext context) {
-    refreshCandidates();
+    refreshTeachers();
     return Scaffold(
       body: ListView.builder(
         itemCount: candidates.length,
